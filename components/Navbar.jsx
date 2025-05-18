58d4b02cc8f6bbd8077c260d76c473d13babd36f
Vikalp Nag <vikalpnag@gmail.com>
2025-05-18 08:42:04 +0530

    add : div

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => (
     whileInView="show"
     className={`${styles.xPaddings} py-8 relative`}
   >
-    navbar
+    <div className="absolute w-[50%] inset-0 gradient-01" />
   </motion.nav>
 );
 
